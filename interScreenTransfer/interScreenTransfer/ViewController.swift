8161bff4bfba92d939f90e6b34ace7f696cd3a8d
yaseminsalann <yaseminsalann@gmail.com>
2025-01-20 21:19:58 +0300

    View Controller yaşam döngüsü
    View Controller yaşam döngüsü bilgileri eklendi.

--- interScreenTransfer/interScreenTransfer/ViewController.swift
+++ interScreenTransfer/interScreenTransfer/ViewController.swift
@@ -15,7 +15,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Görünüm hiyerarşisi belleğe yüklendikten sonra çağrılır.UI elemanlarını yapılandırmak ve başlangıç ayarlarını yapmak için idealdir.
+        
+        //Yaşam Döngüsü Sıralaması
+        //Bir view controller'ın yaşam döngüsü şu sıraya göre işler:
+        //Yaratılma: init()
+        //Yükleme: loadView(), viewDidLoad()
+        //Gösterilme: viewWillAppear(_:), viewDidAppear(_:)
+        //Kaybolma: viewWillDisappear(_:), viewDidDisappear(_:)
+        //Bellek Uyarısı: didReceiveMemoryWarning()
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+     //Görünüm tamamen ekrandan kaybolduktan sonra çağrılır.Belleği boşaltmak veya zamanlayıcıları durdurmak için kullanılabilir.
+        super.viewDidDisappear(animated)
+        print("View Did Disappear çağrıldı.")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+    //Görünüm ekrandan kaybolmadan hemen önce çağrılır.Değişiklikleri kaydetmek veya işlemleri durdurmak için uygundur.
+        super.viewWillDisappear(animated)
+        print("View Will Disappear çağrıldı.")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+    //Görünüm ekranda gösterilmeden hemen önce çağrılır.Görünümün her gösterilişinde yapılması gereken işlemler için kullanılır (örneğin, verileri yenilemek).
+        super.viewWillAppear(animated)
+        print("View Will Appear çağrıldı.")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+    //Görünüm ekranda tamamen görünür hale geldikten sonra çağrılır.Animasyon başlatmak veya kullanıcıdan giriş beklemek için uygundur.
+        super.viewDidAppear(animated)
+        print("View Did Appear çağrıldı.")
+        
     }
     var username =  ""
     @IBAction func nextButton(_ sender: Any) {
